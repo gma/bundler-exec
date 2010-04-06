50515bed624617a11a8c59454a85112fd4b34039
Graham Ashton <graham@effectif.com>
2010-04-06 13:02:34 +0100

    Pass quoted arguments through properly.

--- bundler-exec.sh
+++ bundler-exec.sh
@@ -24,9 +24,9 @@ run-with-bundler()
     local command="$1"
     shift
     if bundler-installed && within-bundled-project; then
-        bundle exec $command $*
+        bundle exec $command "$@"
     else
-        $command $*
+        $command "$@"
     fi
 }
 
