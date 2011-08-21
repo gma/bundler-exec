cf90241f075902be9c33a92bd3736aa8f7e003d2
Graham Ashton <graham@effectif.com>
2011-08-21 06:15:55 +0100

    Added foreman. Reformatted command list.

--- bundler-exec.sh
+++ bundler-exec.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-BUNDLED_COMMANDS="${BUNDLED_COMMANDS:-cap capify cucumber heroku rackup rails rake rspec ruby serve shotgun spec spork thin unicorn unicorn_rails}"
+BUNDLED_COMMANDS="${BUNDLED_COMMANDS:-
+cap
+capify
+cucumber
+foreman
+heroku
+rackup
+rails
+rake
+rspec
+ruby
+serve
+shotgun
+spec
+spork
+thin
+unicorn
+unicorn_rails
+}"
 
 ## Functions
 
