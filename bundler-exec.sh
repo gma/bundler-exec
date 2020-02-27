7096fcbb62ac6ed8ec5ad21be2044e7b69a4bbfa
Michael Grosser <michael@grosser.it>
2020-02-26 21:09:54 -0800

    do not check if bundler is installed ... it always is anyway

--- bundler-exec.sh
+++ bundler-exec.sh
@@ -6,11 +6,6 @@
 
 ## Functions
 
-bundler-installed()
-{
-    which bundle > /dev/null 2>&1
-}
-
 within-bundled-project()
 {
     local dir="$(pwd)"
@@ -23,7 +18,7 @@ within-bundled-project()
 
 run-with-bundler()
 {
-    if bundler-installed && within-bundled-project; then
+    if within-bundled-project; then
         bundle exec "$@"
     else
         "$@"
